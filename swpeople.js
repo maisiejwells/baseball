325d58807f1a31f3907a30be5f64a9e50519b360
Maisie Wells <maisiejwells@gmail.com>
2019-04-11 19:43:11 -0600

    try-stats

--- swpeople.js
+++ swpeople.js
@@ -24,6 +24,8 @@ const allHomeWorlds = people.map(person => {
         name: person.name,
         home: foundWorld.name,
         birth_year: person.birth_year,
+        height: person.height,
+        mass: person.mass,
         imagePath:`https://starwars-visualguide.com/assets/img/characters/${imageURL}.jpg`,
         imagePlanets:`https://starwars-visualguide.com/#/planets/${imageURL}`
     }
@@ -51,7 +53,6 @@ allHomeWorlds.forEach((person) => {
     backImage.src = './starslogo.png'
     cardBack.className = "card__face card__face--back"
     cardBack.appendChild(backImage)
-
 
 
     //front of card
@@ -89,6 +90,7 @@ allHomeWorlds.forEach((person) => {
     document.body.appendChild(mainContainer)
 
 })
+
 
 let SwCard = document.querySelector('.card');
 
@@ -167,64 +169,3 @@ cardContainer.appendChild(imageSaber)
 
 
 
-  // function newCard(name) {
-// let newThing = new Star(name)
-
-// let cardContainer = document.createElement('div')
-// cardContainer.className = "card"
-// cardContainer.addEventListener('click', function() {
-//     console.log('click')
-//     cardContainer.classList.toggle('is-flipped');
-// })
-
-// //back of card
-// let cardBack = document.createElement('div')
-// let backImage = document.createElement('img')
-// backImage.src = './starslogo.png'
-// cardBack.className = "card__face card__face--back"
-// cardBack.appendChild(backImage)
-
-
-
-// //front of card
-// let personElement = document.createElement('div')
-// personElement.className = "card__face card__face--front"
-
-// let planetElement = document.createElement('p')
-// planetElement.className = "planetName"
-// let birthYear = document.createElement('p')
-// let imageElement = document.createElement('img')
-
-// let imagePlanet = document.createElement('img')
-
-// personElement.textContent = person.name
-// planetElement.textContent = person.home
-// birthYear.textContent = person.birth_year
-// imageElement.src = person.imagePath
-
-// imagePlanet.src = person.imagePlanet
-
-// mainContainer.appendChild(cardContainer)
-// cardContainer.appendChild(personElement)
-// cardContainer.appendChild(cardBack)
-
-
-// personElement.appendChild(planetElement)
-// personElement.appendChild(birthYear)
-// personElement.appendChild(imageElement)
-// // mainContainer.appendChild(personElement)
-
-// personElement.appendChild(planetElement)
-// // personElement.appendChild(imagePlanet)
-
-
-// document.body.appendChild(mainContainer)
-
-// }
-
-// let SwCard = document.querySelector('.card');
-
-// SwCard.addEventListener( 'click', function() {
-// console.log('click')
-// SwCard.classList.toggle('is-flipped');
-// });
