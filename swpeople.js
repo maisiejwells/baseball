5154f1d2b2bc0310fd25bfa04e1e31248725b796
Maisie Wells <maisiejwells@gmail.com>
2019-04-11 18:40:16 -0600

    new card

--- swpeople.js
+++ swpeople.js
@@ -2,51 +2,8 @@
 import { people } from './starwars/people.js'
 import { planets } from './starwars/planets.js'
 
-class Star {
-    constructor(name) {
-        this.id = 0,
-        this.name = name
-    }
-}
-
-// let newStar = new Star ('Lightsaber')
-// console.log(newStar)
-
-// const module = document.querySelector('.container')
-
-// function createStarWarsCard(starData) {
-// console.log(starData.id)
-// let card = document.createElement('div')
-// card.className = 'box'
-// let figure = document.createElement('figure')
-// let caption = document.createElement('figcaption')
-// let image = document.createElement('img')
 
 
-// let upperName= starData.name.charAt(0).toUpperCase() + starData.name.slice(1)
-// caption.textContent = upperName
-// if(starData.id !== 0) {
-// image.src = `../images/${starData.id}${upperName}.png`
-// } else {
-// image.src = `../images/lightsaber.jpg`
-// }
-
-// // image.src = starData.sprites.front_shiny
-// figure.appendChild(image)
-// figure.appendChild(caption)
-// card.appendChild(figure)
-// mainContainer.appendChild(card)
-// }
-
-// star.forEach(singleStar => {
-// fetch(sinlgeStar.url)
-// .then(function(response) {
-// return response.wars ()
-// })
-// .then(function(wars) {
-// createStarWarsCard(wars)
-// })
-// })
 
 const getLastNumber = (url) => {
     let end = url.lastIndexOf('/')
@@ -90,7 +47,12 @@ allHomeWorlds.forEach((person) => {
 
     //back of card
     let cardBack = document.createElement('div')
+    let backImage = document.createElement('img')
+    backImage.src = './starslogo.png'
     cardBack.className = "card__face card__face--back"
+    cardBack.appendChild(backImage)
+
+
 
     //front of card
     let personElement = document.createElement('div')
@@ -134,6 +96,7 @@ SwCard.addEventListener( 'click', function() {
     console.log('click')
     SwCard.classList.toggle('is-flipped');
 });
+
 // let test3 = document.createElement('p')
 // test3.textContent = "hello"
 // let testThing = document.querySelector('.card')
@@ -142,7 +105,126 @@ SwCard.addEventListener( 'click', function() {
 const newStarWarsButton = document.querySelector('button')
 
 newStarWarsButton.addEventListener('click', function() {
-createStarWarsCard(newStar)
+let newStarName = prompt('Enter the name of your new Star Wars Character')
+//createStarWarsCard(new Star(newPokeName))
+    newCard(newStarName)
 });
 
 
+class Star {
+    constructor(name) {
+        this.id = 0,
+        this.name = name
+    }
+}
+
+
+function newCard(name) {
+    // let card = document.createElement('div')
+    // card.className = 'card'
+
+
+
+    let cardContainer = document.createElement('div')
+    cardContainer.className = "card"
+    cardContainer.addEventListener('click', function() {
+        console.log('click')
+    cardContainer.classList.toggle('is-flipped');
+})
+
+
+
+//back of card
+let cardBack = document.createElement('div')
+let backImage = document.createElement('img')
+backImage.src = './starslogo.png'
+cardBack.className = "card__face card__face--back"
+cardBack.appendChild(backImage)
+
+mainContainer.appendChild(cardContainer)
+// cardContainer.appendChild(personElement)
+cardContainer.appendChild(cardBack)
+
+// cardContainer.appendChild(imageSaber)
+// personElement.appendChild(planetElement)
+// personElement.appendChild(birthYear)
+// personElement.appendChild(imageElement)
+// mainContainer.appendChild(personElement)
+
+// personElement.appendChild(planetElement)
+// personElement.appendChild(imagePlanet)
+
+
+// document.body.appendChild(mainContainer)
+
+let imageSaber = document.createElement('img')
+imageSaber.className = "saberpic"
+imageSaber.src = "./images/lightsaber.jpg"
+cardContainer.appendChild(imageSaber)
+
+}
+
+
+
+
+  // function newCard(name) {
+// let newThing = new Star(name)
+
+// let cardContainer = document.createElement('div')
+// cardContainer.className = "card"
+// cardContainer.addEventListener('click', function() {
+//     console.log('click')
+//     cardContainer.classList.toggle('is-flipped');
+// })
+
+// //back of card
+// let cardBack = document.createElement('div')
+// let backImage = document.createElement('img')
+// backImage.src = './starslogo.png'
+// cardBack.className = "card__face card__face--back"
+// cardBack.appendChild(backImage)
+
+
+
+// //front of card
+// let personElement = document.createElement('div')
+// personElement.className = "card__face card__face--front"
+
+// let planetElement = document.createElement('p')
+// planetElement.className = "planetName"
+// let birthYear = document.createElement('p')
+// let imageElement = document.createElement('img')
+
+// let imagePlanet = document.createElement('img')
+
+// personElement.textContent = person.name
+// planetElement.textContent = person.home
+// birthYear.textContent = person.birth_year
+// imageElement.src = person.imagePath
+
+// imagePlanet.src = person.imagePlanet
+
+// mainContainer.appendChild(cardContainer)
+// cardContainer.appendChild(personElement)
+// cardContainer.appendChild(cardBack)
+
+
+// personElement.appendChild(planetElement)
+// personElement.appendChild(birthYear)
+// personElement.appendChild(imageElement)
+// // mainContainer.appendChild(personElement)
+
+// personElement.appendChild(planetElement)
+// // personElement.appendChild(imagePlanet)
+
+
+// document.body.appendChild(mainContainer)
+
+// }
+
+// let SwCard = document.querySelector('.card');
+
+// SwCard.addEventListener( 'click', function() {
+// console.log('click')
+// SwCard.classList.toggle('is-flipped');
+// });
